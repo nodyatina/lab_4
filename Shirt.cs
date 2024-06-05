23ba402cc84a439cdae8686bc3e04d57f976d1a3
nodyatina <167766275+nodyatina@users.noreply.github.com>
2024-06-05 21:38:11 +0300

    Update Shirt.cs

--- Shirt.cs
+++ Shirt.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace LinQ{
-    public class Shirt
-{
-    public double SleeveLength { get; set; }
-    public decimal Cost { get; set; }
-    public int Size { get; set; }
-
-    public Shirt(double sleeveLength, decimal cost, int size)
+    public class Shirt : Clothing
     {
-        SleeveLength = sleeveLength;
-        Cost = cost;
-        Size = size;
+        public double SleeveLength { get; set; }
+        public int Size { get; set; }
+
+        public Shirt(double sleeveLength, decimal cost, int size) : base(cost)
+        {
+            SleeveLength = sleeveLength;
+            Size = size;
+        }
     }
-}
-}
