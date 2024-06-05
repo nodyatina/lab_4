94b4c51a3fb26c35a6c4cc641044542f0b1f9bec
nodyatina <167766275+nodyatina@users.noreply.github.com>
2024-06-05 21:36:10 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,37 +2,47 @@
 using System.Collections.Generic;
 using System.Linq; 
 
-namespace LinQ{ 
-    class Program
+namespace LinQ
 {
-    static void Main() 
+    class Program
     {
-        
-        var clothes = new List<object> 
+        static void Main()
         {
-            new Pants("Denim", 1200, 32), 
-            new Pants("Cotton", 900, 30),
-            new Shirt(33.5, 850, 40),
-            new Shirt(34, 870, 39),
-            new Pants("Wool", 1600, 34)
-        };
+            var clothes = new List<Clothing>
+            {
+                new Pants("Denim", 1200, 32),
+                new Pants("Cotton", 900, 30),
+                new Shirt(33.5, 850, 40),
+                new Shirt(34, 870, 39),
+                new Pants("Wool", 1600, 34)
+            };
 
-        
-        var query = clothes 
-        .OfType<dynamic>() 
-        .GroupBy(c => c.GetType().Name)
-        .Select(g => new
-    {
-        Type = g.Key,
-        TotalCost = g.Sum(c => (decimal)c.Cost)
-    });
+            var query = clothes
+                .GroupBy(c => c.GetType().Name)
+                .Select(g => new
+                {
+                    Type = g.Key,
+                    TotalCost = g.Sum(c => c.Cost)
+                });
 
-       
-        foreach (var group in query)
-        {
-            Console.WriteLine($"Тип: {group.Type}, Суммарная стоимость: {group.TotalCost}");
-        }
+            foreach (var group in query)
+            {
+                Console.WriteLine($"Type: {group.Type}, Total Cost: {group.TotalCost}");
+            }
             Console.ReadKey();
+        }
     }
-}
-}
+
+    public abstract class Clothing
+    {
+        public decimal Cost { get; set; }
+
+        protected Clothing(decimal cost)
+        {
+            if (cost < 0)
+            {
+                throw new ArgumentException("Cost cannot be negative");
+            }
+            Cost = cost;
+        }
+    }
